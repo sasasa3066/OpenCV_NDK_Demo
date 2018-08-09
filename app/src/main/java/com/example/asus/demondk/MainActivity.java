32b0065797da2ee40bb97d66483a169767af4869
d0584776 <40332248@gm.nfu.edu.tw>
2018-08-09 17:05:36 +0800

    Android use NDK with OpenCV.

---
+++ app/src/main/java/com/example/asus/demondk/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.asus.demondk;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("testMessage");
+        System.loadLibrary("testOpenCv");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        ImageView preImg=(ImageView)findViewById(R.id.img1);
+        ImageView pastImg=(ImageView)findViewById(R.id.img2);
+        Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.drawable.test11);
+        int w = bitmap.getWidth(), h = bitmap.getHeight();
+        int[] pix = new int[w * h];
+        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+        int [] resultPixes=gray(pix,w,h);
+        Bitmap result = Bitmap.createBitmap(w,h, Bitmap.Config.RGB_565);
+        result.setPixels(resultPixes, 0, w, 0, 0,w, h);
+        preImg.setImageBitmap(bitmap);
+        pastImg.setImageBitmap(result);
+        //
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native String getTestString();
+    public static native int[] gray(int[] buf, int w, int h);
+}
